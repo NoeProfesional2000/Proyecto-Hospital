ef1d4edfcc4fa89936d4334a0f4627a102281cd5
NoeProfesional2000 <85148283+NoeProfesional2000@users.noreply.github.com>
2021-11-08 13:20:49 -0800

    Add files via upload

--- ProcedimientosAlmacenados.sql
+++ ProcedimientosAlmacenados.sql
@@ -5,6 +5,7 @@ DROP FUNCTION Requiriente(opcion INTEGER, nombre VARCHAR, descrip VARCHAR);
 CREATE OR REPLACE FUNCTION Requiriente(opcion INTEGER, nombre VARCHAR, descrip VARCHAR) RETURNS VARCHAR AS $BODY$
 DECLARE
     Consulta INTEGER := (SELECT id_area_requiriente FROM area_requiriente WHERE UPPER(nombre_area) = UPPER(nombre));
+    Baja_repetida INTEGER;
 BEGIN
     CASE (opcion)
     --- Si se recibe el número 1 como parámetro, se da de Alta ---
@@ -18,8 +19,13 @@ BEGIN
      --- Si se recibe el número 2 como parámetro, se da de Baja ---
         WHEN (2) THEN
             IF Consulta > 0 THEN
-                UPDATE area_requiriente SET estatus = 0 WHERE UPPER(nombre_area) = UPPER(nombre);
-                RETURN 'DADO DE BAJA';
+                Baja_repetida := (SELECT estatus FROM area_requiriente WHERE UPPER(nombre_area) = UPPER(nombre));
+                IF Baja_repetida = 0 THEN
+                    RETURN 'YA HA SIDO DE BAJA CON ANTERIORIDAD';
+                ELSE
+                    UPDATE area_requiriente SET estatus = 0 WHERE UPPER(nombre_area) = UPPER(nombre);
+                    RETURN 'DADO DE BAJA';
+                END IF;
             ELSE
                 RETURN 'NO EXISTE EL AREA';
             END IF;
@@ -38,6 +44,9 @@ EXCEPTION
 END;
 $BODY$ LANGUAGE 'plpgsql';
 
+
+--DETALLE PEDIDO
+-- PASAR ID_PEDIDOS DESPACHADOS, CADENA PARA INSERTAR EN ALMACEN Y CADENA DE UPDATES EN STOCK
 
 -- CREANDO EL PL DE INSERCION.
 DROP FUNCTION Insertar_Detalle(id INTEGER, cadena_restante VARCHAR);
@@ -59,6 +68,76 @@ EXCEPTION
     WHEN SQLSTATE '42830' THEN
         DELETE FROM pedidos WHERE id_pedidos = id;
         RETURN 'ERROR, ALGO NO COINCIDE';
+    ROLLBACK;
+    COMMIT;
+END;
+$BODY$ LANGUAGE 'plpgsql';
+
+
+-- ALMACEN
+-- CREANDO EL PL DE INSERCION.
+DROP FUNCTION Insertar_Almacen(id INTEGER, cadena_insertar VARCHAR, cadena_restar VARCHAR);
+CREATE OR REPLACE FUNCTION Insertar_Almacen(id INTEGER, cadena_insertar VARCHAR, cadena_restar VARCHAR) RETURNS VARCHAR AS $BODY$
+DECLARE
+    fecha_insercion DATE := (SELECT CURRENT_DATE);
+    hora_insercion TIME := (SELECT CURRENT_TIME);
+    verificar_actualizacion INTEGER;
+    verificar_resta INTEGER;
+    variable VARCHAR;
+BEGIN
+    
+    -- CAMBIAMOS ESTATUS EN PEDIDOS DESPACHADOS
+    UPDATE pedidos_despachados SET estatus = 'realizado', fecha_recibido = fecha_insercion, hora_recibido = hora_insercion WHERE id_pedidos_despachados = id;
+
+    --SI OBTENGO 0, ERROR, SI OBTENGO 1 SIGUE
+    verificar_actualizacion := (SELECT COUNT(*) FROM pedidos_despachados WHERE id_pedidos_despachados=id AND estatus = 'realizado');
+
+    -- SI ES CERO, LO QUIEBRO Y ASIGNO VALOR
+    IF verificar_actualizacion = 0 THEN
+        variable := 'ERROR EN ACTUALIZAR';
+        EXECUTE 'INSERT INTO almacen(id_pedidos_despachados, id_insumos,piezas) VALUES (0,0,3)';
+    END IF;
+
+    -- SI POR ERROR SE ASIGNA UN INSUMO NO EXISTENTE, SE VA A QUEBRAR
+    -- INSERTAMOS EN DETALLE ALMACEN
+    EXECUTE 'INSERT INTO almacen(id_pedidos_despachados, id_insumos,piezas) VALUES ' || cadena_insertar;
+
+    -- SI TODO ESTA CORRECTO RESTAMOS
+    --DESCONTAMOS DE INSUMOS
+    EXECUTE cadena_restar;
+
+    -- SI EXISTE ALGUN NUMERO NEGATIVO HUBO UN ERROR Y QUEBRAMOS
+    verificar_resta := (SELECT COUNT(*) FROM insumos WHERE stock < 0);
+    IF verificar_resta > 0 THEN
+        variable := 'EL STOCK PEDIDO, SOBREPASA AL EXISTENTE';
+        EXECUTE 'INSERT INTO almacen(id_pedidos_despachados, id_insumos,piezas) VALUES (0,0,3)';
+    ELSE
+        variable := 'PEDIDO RECIBIDO CORRECTAMENTE';
+    END IF;
+
+    RETURN variable;
+EXCEPTION
+    WHEN SQLSTATE '23503' THEN
+        IF variable = 'EL STOCK PEDIDO, SOBREPASA AL EXISTENTE' THEN
+            RETURN 'EL STOCK PEDIDO, SOBREPASA AL EXISTENTE';
+        ELSE 
+            IF variable = 'ERROR EN ACTUALIZAR' THEN
+                RETURN 'ERROR EN ACTUALIZAR EL ESTATUS PEDIDO';
+            ELSE
+                RETURN 'ERROR, ALGO NO COINCIDE';
+            END IF;
+        END IF;
+    WHEN SQLSTATE '42830' THEN
+        IF variable = 'EL STOCK PEDIDO, SOBREPASA AL EXISTENTE' THEN
+            RETURN 'EL STOCK PEDIDO, SOBREPASA AL EXISTENTE';
+        ELSE 
+            IF variable = 'ERROR EN ACTUALIZAR' THEN
+                RETURN 'ERROR EN ACTUALIZAR EL ESTATUS PEDIDO';
+            ELSE
+                RETURN 'ERROR, ALGO NO COINCIDE';
+            END IF;
+        END IF;
+
     ROLLBACK;
     COMMIT;
 END;
